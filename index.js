334a887efb42acc727b1ff6a0edd1eb422335cda
Fienix <federico.fieni@gmail.com>
2017-01-25 16:15:19 +0100

    Add writematadata plugin

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 var Metalsmith  = require('metalsmith'),
-markdown    = require('metalsmith-markdown'),
-layouts     = require('metalsmith-layouts'),
-permalinks  = require('metalsmith-permalinks'),
-drafts      = require('metalsmith-drafts');
+markdown        = require('metalsmith-markdown'),
+layouts         = require('metalsmith-layouts'),
+permalinks      = require('metalsmith-permalinks'),
+drafts          = require('metalsmith-drafts'),
+writemetadata   = require('metalsmith-writemetadata');
 
 
 Metalsmith(__dirname)
@@ -20,6 +21,11 @@ Metalsmith(__dirname)
 .use(layouts({
   engine: 'handlebars',
   partials: 'partials'
+}))
+.use(writemetadata({
+  pattern: ['**/*'],
+  ignorekeys: ['next','previous'],
+  bufferencoding: 'utf8'
 }))
 .build(function(err, files){
   if (err) { throw err; }
