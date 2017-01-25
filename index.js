272028574062ba130ee1a589a859755c0ce7d424
Fienix <federico.fieni@gmail.com>
2017-01-25 18:06:13 +0100

    Add debug plugin

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ markdown        = require('metalsmith-markdown'),
 layouts         = require('metalsmith-layouts'),
 permalinks      = require('metalsmith-permalinks'),
 drafts          = require('metalsmith-drafts'),
-writemetadata   = require('metalsmith-writemetadata');
-
+writemetadata   = require('metalsmith-writemetadata'),
+debug           = require('metalsmith-debug');
 
 Metalsmith(__dirname)
 .metadata({
@@ -33,6 +33,14 @@ Metalsmith(__dirname)
   pattern: ['**/*'],
   // ignorekeys: ['next','previous'],
   bufferencoding: 'utf8'
+}))
+.use(debug({ // Use setting up an environment variable DEBUG=metalsmith:*
+  log: "first debug",      // any comment you like
+  metadata: false,         // default: true
+  source: false,           // default: true
+  destination: false,      // default: true
+  files: true,             // default: true
+  match: "**/*.md"         // default: all files
 }))
 .build(function(err, files){
   if (err) { throw err; }
