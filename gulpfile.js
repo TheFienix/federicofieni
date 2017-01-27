7aeb7fe4aeb2f1b3197dfa5c55577dc37435fddc
Fienix <federico.fieni@gmail.com>
2017-01-28 00:34:27 +0100

    Remove autoprefixer, add sourcemaps

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,17 @@
 var gulp = require('gulp'),
-    postcss = require('gulp-postcss'),
-    cssnext = require('postcss-cssnext'),
-    autoprefixer = require('autoprefixer'),
-    precss = require('precss')
-    ;
-
+postcss = require('gulp-postcss'),
+sourcemaps = require('gulp-sourcemaps'),
+cssnext = require('postcss-cssnext'),
+precss = require('precss')
+;
 
 gulp.task('css',function () {
-  var processors = [
-    autoprefixer({browsers: ['last 1 version']}),
-    cssnext,
-    precss
-  ];
   gulp.src('./src/styles/*.css')
-  .pipe(postcss(processors))
+  .pipe(sourcemaps.init())
+  .pipe(postcss([
+    precss,
+    cssnext({browsers: ['last 1 version']})
+  ]))
+  .pipe(sourcemaps.write())
   .pipe(gulp.dest('./docs/css'));
 });
