055ad06d9596df4259fa225fed145b17b137f7df
Fienix <federico.fieni@gmail.com>
2017-01-28 15:24:22 +0100

    Change plugin order

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,31 @@
 var gulp = require('gulp'),
 postcss = require('gulp-postcss'),
-sourcemaps = require('gulp-sourcemaps'),
 cssnext = require('postcss-cssnext'),
+sourcemaps = require('gulp-sourcemaps'),
 precss = require('precss')
 ;
 
 gulp.task('css',function () {
+
+  var plugins = [
+    cssnext,
+    precss
+  ];
+
   gulp.src('./src/styles/*.css')
   .pipe(sourcemaps.init())
-  .pipe(postcss([
-    precss,
-    cssnext({browsers: ['last 1 version']})
-  ]))
-  .pipe(sourcemaps.write())
+  .pipe(postcss(plugins))
+  .pipe(sourcemaps.write('.'))
   .pipe(gulp.dest('./docs/css'));
 });
+
+gulp.task('js',function(){
+
+});
+
+gulp.task('watch',function(){
+  gulp.watch('./src/styles/*.css',['css']);
+  gulp.watch('./src/scripts/*.js',['js']);
+});
+
+gulp.task('default',['watch']);
