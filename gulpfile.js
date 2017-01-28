b491afd1792ae4465019cd6c1960faa03b3f3330
Fienix <federico.fieni@gmail.com>
2017-01-28 16:58:54 +0100

    Add browsersync life reload

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,9 @@ var gulp = require('gulp'),
 postcss = require('gulp-postcss'),
 cssnext = require('postcss-cssnext'),
 sourcemaps = require('gulp-sourcemaps'),
-precss = require('precss')
+precss = require('precss'),
+concat = require('gulp-concat'),
+browserSync = require('browser-sync').create()
 ;
 
 gulp.task('css',function () {
@@ -11,21 +13,36 @@ gulp.task('css',function () {
     cssnext,
     precss
   ];
-
-  gulp.src('./src/styles/*.css')
+  console.log('inject css');
+  return gulp.src('./src/styles/*.css')
   .pipe(sourcemaps.init())
   .pipe(postcss(plugins))
   .pipe(sourcemaps.write('.'))
-  .pipe(gulp.dest('./docs/css'));
+  .pipe(gulp.dest('./docs/css'))
+  .pipe(browserSync.stream());
 });
 
 gulp.task('js',function(){
-
+  var dependencies = [];
+  return gulp.src(['src/scripts/*.js'])
+  .pipe(concat('scripts.js'))
+  .pipe(gulp.dest('docs/js'));
 });
 
-gulp.task('watch',function(){
+gulp.task('js-watch',['js'],function(done){
+  browserSync.reload();
+  done();
+});
+
+gulp.task('serve',function(){
+
+  browserSync.init({
+    server: "./docs"
+  });
+
   gulp.watch('./src/styles/*.css',['css']);
-  gulp.watch('./src/scripts/*.js',['js']);
+  gulp.watch('./src/scripts/*.js',['js-watch']);
+
 });
 
-gulp.task('default',['watch']);
+gulp.task('default',['serve']);
