9bada7ca0d5c200dcb77c6f1074c3d389bb18a1f
Fienix <federico.fieni@gmail.com>
2017-05-07 13:32:49 +0200

    Remove metalsmith specific gulp plugins

--- gulpfile.js
+++ gulpfile.js
@@ -1,29 +1,18 @@
 var
 assign          = require('lodash.assign'),
 browserSync     = require('browser-sync').create(),
-collections     = require('metalsmith-collections'),
 concat          = require('gulp-concat'),
 cssnano         = require('gulp-cssnano'),
 cssnext         = require('postcss-cssnext'),
-debug           = require('metalsmith-debug'),
 del             = require('del'),
-drafts          = require('metalsmith-drafts'),
 fingerprint     = require('gulp-fingerprint'),
 gulp            = require('gulp'),
 gulpFrontMatter = require('gulp-front-matter'),
-gulpsmith       = require('gulpsmith'),
-images          = require('metalsmith-project-images'),
-layouts         = require('metalsmith-layouts'),
-markdown        = require('metalsmith-markdown'),
-metalsmith      = require('gulp-metalsmith'),
-metalsmithConf = require('./metalsmith.conf'),
-permalinks      = require('metalsmith-permalinks'),
 plumber         = require('gulp-plumber'),
 postcss         = require('gulp-postcss'),
 precss          = require('precss'),
 rev             = require('gulp-rev'),
-sourcemaps      = require('gulp-sourcemaps'),
-writemetadata   = require('metalsmith-writemetadata')
+sourcemaps      = require('gulp-sourcemaps')
 ;
 
 gulp.task('css',function () {
