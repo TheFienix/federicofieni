63ae564408541b180aa29928f8a38a00dcac7af1
Fienix <federico.fieni@gmail.com>
2017-06-03 19:31:43 +0200

    move preventedfault

--- scripts/scroll.js
+++ scripts/scroll.js
@@ -2,11 +2,11 @@
 
   $( window ).on('mousewheel', function(event) {
     if ( window.innerWidth <= 800 ) return;
+    event.preventDefault();
     var $window = $( window );
     var currentScrollPosition =  $window.scrollLeft();
     var direction = event.originalEvent.wheelDelta < 0 ? -1 : 1;
     $(this).scrollLeft( (currentScrollPosition -= ( 45 * direction )) );
-    event.preventDefault();
   });
 
 })(jQuery);
