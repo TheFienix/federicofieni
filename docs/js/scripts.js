e38793378f03a62840d8205c65a6605f91cbd4a1
Fienix <federico.fieni@gmail.com>
2018-01-02 17:08:10 +0100

    Build website with grid

--- docs/js/scripts.js
+++ docs/js/scripts.js
@@ -103,18 +103,3 @@ null==d?void 0:d))},attrHooks:{type:{set:function(a,b){if(!o.radioValue&&"radio"
     exports.reset = reset;
 }));
 
-
-(function($){
-
-  var dragscroll = document.querySelectorAll('.dragscroll');
-  if ( dragscroll.length === 0 ) return;
-
-  $( window ).on('mousewheel', function(event) {
-    if ( window.innerWidth <= 800 ) return;
-    // event.preventDefault();
-    var $window = $( window );
-    var currentScrollPosition =  $window.scrollLeft();
-    $(this).scrollLeft( (currentScrollPosition -= ( 45 * event.deltaY )) );
-  });
-
-})(jQuery);
